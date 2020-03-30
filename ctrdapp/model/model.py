e71eb0d7e5c6660600f052dd393c51abcd755595
Conor Messer <messer.co@husky.neu.edu>
2020-03-30 15:22:05 -0600

    Jacobian only calculated if q_dot is non-zero for efficiency

--- ctrdapp/model/model.py
+++ ctrdapp/model/model.py
@@ -108,20 +108,22 @@ class Model:
             eta_tr1 = big_adjoint(g_theta) * delta_theta[n] @ x_axis_unit
             eta_tr2 = big_adjoint(g_theta) * velocity @ ksi_here
 
-            this_q_dot = [0] * self.q_dof
             if self.q_dot_bool:
                 this_q_dot = None  # todo calculate q_dot - should be array with size q_dof
-            jacobian_r_init = np.zeros([6, self.q_dof])
-            eta_cr_o = jacobian_r_init @ this_q_dot
+                jacobian_r_init = np.zeros([6, self.q_dof])
+                eta_cr_here = jacobian_r_init @ this_q_dot
+                jacobian_r_previous = jacobian_r_init
+            else:
+                eta_cr_here = 0
 
-            eta_r_here = eta_previous_tube + eta_tr1 + eta_tr2 + eta_cr_o
+            eta_r_here = eta_previous_tube + eta_tr1 + eta_tr2 + eta_cr_here
 
             this_g = [g_initial]
             this_ksi_c = [ksi_here]
             this_eta_r = [eta_r_here]
 
             g_previous = g_initial
-            jacobian_r_previous = jacobian_r_init
+
             for i in range(insert_index + 1, self.num_discrete_points):
                 this_base = self.strain_base[n](self.delta_x * i, self.q_dof)
                 centered_s = (i - 0.5) * self.delta_x
@@ -135,17 +137,17 @@ class Model:
                                           self.delta_x * hat(ksi_here_center))
                 g_here = g_previous @ gn_here
 
-                jacobian_r_here = jacobian_r_previous + big_adjoint(g_previous) @ t_exponential(self.delta_x, norm_k_here_center, ksi_here_center) @ this_centered_base
-                eta_cr_here = jacobian_r_here @ this_q_dot
-                eta_r_here = eta_previous_tube + eta_tr1 + eta_tr2 + eta_cr_here
+                if self.q_dot_bool:
+                    jacobian_r_here = jacobian_r_previous + big_adjoint(g_previous) @ t_exponential(self.delta_x, norm_k_here_center, ksi_here_center) @ this_centered_base
+                    eta_cr_here = jacobian_r_here @ this_q_dot
+                    jacobian_r_previous = jacobian_r_here
+                    eta_r_here = eta_previous_tube + eta_tr1 + eta_tr2 + eta_cr_here
+                    this_eta_r.append(eta_r_here)
 
-                # duplicate code!!!!*****
                 this_g.append(g_here)
                 this_ksi_c.append(ksi_here)
-                this_eta_r.append(eta_r_here)
 
                 g_previous = g_here
-                jacobian_r_previous = jacobian_r_here
             eta_previous_tube = big_adjoint(np.linalg.inv(g_previous)) @ eta_r_here
             g_out.append(this_g)
             eta_out.append(this_eta_r)
