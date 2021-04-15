17a1456e7172faf414aac2b1ad7b22f2f0c5eed7
Conor Messer <messer.co@husky.neu.edu>
2021-04-15 09:11:43 -0400

    Cleaned up code; still needs better user input for configuration.

--- ctrdapp/scripts/output_path.py
+++ ctrdapp/scripts/output_path.py
@@ -1,33 +1,33 @@
 from ctrdapp.model.model_factory import create_model
 from ctrdapp.solve.visualize_utils import visualize_curve
-from .setup_script import setup_script
+from ctrdapp.scripts.setup_script import setup_script
 
 
-def main():
+def output_path():
+    """Script allows for visualizing tubes in time with specified insertion/rotations.
+
+    Returns
+    -------
+
+    """
     collision_detector, objects_file, heuristic_factory, this_q, configuration, output_path = setup_script()
 
     this_model = create_model(configuration, this_q)
 
-    all_curves = [this_model.solve_g(indices=[50], thetas=[0], full=False)]
+    # this is the g_curve for the initial position
+    all_curves = [this_model.solve_g(indices=[7, 15], thetas=[0, 0], full=False)]
 
-    for i in range(1, 50):
+    # this iterates through many configurations; can be changed to get desired motion
+    for i in range(1, 250):
         g_out, eta_out, new_insert_indices, true_insertions, ftl_out = \
-            this_model.solve_integrate([0], [1], [0], [i], all_curves[i - 1])
+            this_model.solve_integrate([0, 0.01], [0, 0], [0, 0.01 * i], [35, 75], all_curves[i - 1], invert_insert=False)
 
-    # Small insertion w/ rotation
-    # g_out, eta_out, new_insert_indices, true_insertions, ftl_out = \
-    #     this_model.solve_integrate([-0.1], [1], [0], [50], all_curves[0])
-
-    # Small insertion
-    # g_out, eta_out, new_insert_indices, true_insertions, ftl_out = \
-    #     this_model.solve_integrate([0], [1], [0], [50], all_curves[0])
         all_curves.append(g_out)
 
-        heuristic = heuristic_factory.create(follow_the_leader=ftl_out)
-
-    visualize_curve(all_curves, objects_file, configuration.get("tube_number"), configuration.get("tube_radius"),
+    rad = configuration.get("tube_radius")
+    visualize_curve(all_curves, objects_file, configuration.get("tube_number"), rad.get("outer"),
                     output_path, "FTL_linear_insertion_animated")
 
 
 if __name__ == "__main__":
-    main()
+    output_path()
