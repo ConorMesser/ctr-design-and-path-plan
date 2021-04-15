ace238654b9c0c58f8192390800eba8c84a57fdf
Conor Messer <messer.co@husky.neu.edu>
2021-04-15 09:11:15 -0400

    Allow for plotting multiple tubes and printing g_curve to txt with user input

--- ctrdapp/scripts/output_curves.py
+++ ctrdapp/scripts/output_curves.py
@@ -8,33 +8,31 @@ import numpy as np
 
 
 def main():
-    collision_detector, objects_file, heuristic_factory, this_q, configuration, output_path = setup_script()
+    plotter = None
+    i = 0
+    save_curves = input("Save ng_curves in txt file? (y/n)")
 
-    # create model
-    this_model = create_model(configuration, this_q)
+    while True:
+        collision_detector, objects_file, heuristic_factory, this_q, configuration, output_path = setup_script()
 
-    try_q = np.array([-1.01338092e+01,  1.16295691e+00, -1.98104425e-02,  9.19414724e-05,
-                      6.01577902e+00,  3.81962133e+00, -7.62825629e-02,  3.74874061e-04,
-                      -1.05176911e+00,  1.02050308e-01, -1.68539548e-03,  7.70259132e-06,
-                      2.69890376e-01,  6.16774319e-02, -1.54744257e-03,  9.12084355e-06,
-                      3.06374117e+00, -6.28190146e-02, -3.65086842e-05,  2.66174270e-06,
-                      2.65416918e-02, -4.47108927e-04,  1.85629186e-06, -7.49269565e-10,
-                      -5.73181894e-02,  2.17707911e-03, -2.02683214e-05,  6.09485549e-08,
-                      2.54604827e-02, -1.09619732e-03,  1.33477319e-05, -4.86996371e-08,
-                      -2.14492224e-01,  2.81060637e-03, -1.10926421e-05,  1.30145562e-08,
-                      3.00000000e-02])
+        # create model
+        this_model = create_model(configuration, this_q)
 
-    try_q = np.zeros(13)
-    try_q[3] = try_q[6] = try_q[12] = 0.01
+        # todo get input from user
+        this_g = this_model.solve_g(indices=[3, 9], thetas=[3.6609270062908728, 1.3051197304814763], full=False)
 
-    # todo get input from user
-    this_g = this_model.solve_g(indices=[0], thetas=[0], full=True)  # , initial_guess=try_q)  # , full=True) initial_guess=try_q)
-    # g_filename = output_path / "g_curves.txt"
-    # save_g_positions(this_g, g_filename)
+        if save_curves == 'y' or save_curves == 'yes':
+            g_filename = output_path / f"g_curves_{i}.txt"
+            save_g_positions(this_g, g_filename)
 
-    visualize_curve_single(this_g, objects_file, configuration.get("tube_number"),
-                           configuration.get("tube_radius").get("outer"),
-                           output_path, "curve_visual")
+        t_col = input("Tube color for this run: ")
+        plotter = visualize_curve_single(this_g, objects_file, configuration.get("tube_number"),
+                                         configuration.get("tube_radius").get("outer"), output_path, "curve_visual",
+                                         old_plotter=plotter, tube_color=t_col)
+
+        addl_tube = input('Is viewing an additional tube desired? (y/n)')
+        if addl_tube != 'y' or addl_tube != 'yes':
+            break
 
 
 if __name__ == "__main__":
